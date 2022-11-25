27853c1e3b9bdcd8842be26bac1c409c87fba8ed
aleksndr <ostraleks@gmail.com>
2022-11-25 22:12:23 +0500

    completed Task2

---
+++ src/org/gigbrains/Task2.java
@@ -0,0 +1,10 @@
+package org.gigbrains;
+
+public class Task2 {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < ans.length; i++)
+            ans[i]+=nums[nums[i]];
+        return ans;
+    }
+}
